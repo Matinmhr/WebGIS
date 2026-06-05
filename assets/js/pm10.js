4cddae13fa3301f50c54ed9ed3a7c48ef30be73b
mariorossimr060 <mariorossimr060@gmail.com>
2026-06-05 12:12:40 +0200

    Add files via upload

---
+++ assets/js/pm10.js
@@ -0,0 +1,9 @@
+const tableRows = [
+  ['Stability / Persistence', '95.21%', 'Trees → Trees'],
+  ['Primary Source of Gain', '48.79%', 'Rangeland → Trees'],
+  ['Secondary Source of Gain', '42.07%', 'Crops → Trees'],
+  ['Third Source of Gain', '7.19%', 'Built Area → Trees'],
+  ['Primary Destination of Loss', '43.92%', 'Trees → Crops'],
+  ['Secondary Destination of Loss', '27.78%', 'Trees → Built Area'],
+  ['Third Destination of Loss', '26.00%', 'Trees → Rangeland']
+];
